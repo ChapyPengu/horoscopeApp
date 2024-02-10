3e0bccc2514c8ae600e1326cfb86902db4f214a2
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2024-02-10 14:36:00 -0300

    first commit

---
+++ app/src/main/java/com/example/horoscoapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.horoscoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
+        navController = navHost.navController
+        binding.bnv.setupWithNavController(navController)
+    }
+
+
+
+}
